312436a33df7f3725a9040a9b76f8e40fdadd411
ThomasMcAndrew <thomasmca86@gmail.com>
2022-12-13 06:00:46 -0600

    adding help tag

--- EasyCLI/Attributes.cs
+++ EasyCLI/Attributes.cs
@@ -1,17 +1,30 @@
 namespace EasyCLI;
 
+public class HelpAttribute : System.Attribute 
+{
+	public string? Value { get; }
+	public HelpAttribute(string val)
+	{
+        // I want to be able to format text how I want
+        // without having to have it all the way to the other 
+        //side so this will remove whitespace before the paragraph
+		Value = val.Split('\n')
+            .Select(x => x.Trim())
+            .Aggregate((x,y) => $"{x}\n{y}");
+	}
+}
 public abstract class FlagAttribute : System.Attribute 
 {
     public string[] Flags { get; }
     public FlagAttribute(params string[] flags) { Flags = flags; }
 }
 
-public class Manditory : FlagAttribute
+public class ManditoryAttribute : FlagAttribute
 { 
     public Manditory(params string[] flags) : base(flags) {}
 }
 
-public class Either : FlagAttribute
+public class EitherAttribute : FlagAttribute
 {   //Should be able to accept a key or string or something so you can have multiple eithers. 
     public Either(params string[] flags) : base(flags) {}
     //like this then I guess it could be passed to flag attribute or just in this one, 
@@ -20,9 +33,9 @@ public class Either : FlagAttribute
     //mandatory could be like a guid for each creation
     //and not required could just be an empty string.
     //With this tho error messages wouldnt be able to be as percise so..
-    public Either(string id, params string[] flags) : base(flags) {}
+    public Either(Guid id, params string[] flags) : base(flags) {}
 }
-public class NotRequired : FlagAttribute
+public class NotRequiredAttribute : FlagAttribute
 { 
     public NotRequired(params string[] flags) : base(flags) {}
 }
